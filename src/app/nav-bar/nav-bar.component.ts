8282cddbea65f366653e9d863bc2cf5be812b0e1
Alex Nico <a.nicolaidis@outlook.com>
2025-05-29 19:09:46 +1000

    WIP: responsive page

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { NameComponent } from '../name/name.component';
 import { NavigationLinksComponent } from '../navigation-links/navigation-links.component';
 import { SocialsComponent } from '../socials/socials.component';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [NameComponent, NavigationLinksComponent, SocialsComponent],
+  imports: [NavigationLinksComponent, SocialsComponent],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css',
 })
